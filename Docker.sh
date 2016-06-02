d992d39c64e0dcfd9b9b2271c2de13e6e77e1e23
Supermies <pohjola.ville@suomi24.fi>
2016-06-02 10:15:41 +0300

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -32,7 +32,7 @@ vgcreate VG /dev/sdb6
 pvcreate /dev/sdb7
 vgcreate VG /dev/sdb7
 lvcreate -l 100%FREE -n levy VG -y
-mkfs  -t ext4 /dev/VG/levy -y
+mkfs  -t ext3 /dev/VG/levy -y
 mkdir /kansio
 mount /dev/VG/levy /kansio
 sudo apt-get update
