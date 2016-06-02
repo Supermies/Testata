84174c3d53baf927dcf22516d51854a640eaa9a5
Supermies <pohjola.ville@suomi24.fi>
2016-06-02 14:50:16 +0300

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -41,6 +41,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
 mkdir /etc/apt
 mkdir /etc/apt/sources.list.d
 sudo touch /etc/apt/sources.list.d/docker.list
