1e0e1fa08e5bf881b4704b4ebda6cd741124c583
Supermies <pohjola.ville@suomi24.fi>
2016-06-14 14:41:19 +0300

    Update DockerNA.sh

--- DockerNA.sh
+++ DockerNA.sh
@@ -19,4 +19,4 @@ sudo apt-get update
 sudo apt-get install docker-engine -y
 sudo usermod -aG docker root
 sudo usermod -aG docker asd
-sg docker -c docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.0.1 http://192.168.1.3:8080/v1/scripts/8FD1B57205383841D6C6:1465462800000:HtjAOzNyynZtDgos5TDYFCO00
+sg docker -c "docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.0.1 http://192.168.1.3:8080/v1/scripts/8FD1B57205383841D6C6:1465902000000:K7K3C5C5hUHt4yeP3PIO79ELsc"
