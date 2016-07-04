7ab363d5a9856bfb140af2b37af931547a4643a7
Supermies <pohjola.ville@suomi24.fi>
2016-07-04 02:58:43 -0400

    Update DockerNA.sh

--- DockerNA.sh
+++ DockerNA.sh
@@ -5,7 +5,6 @@ sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo mkdir /etc/apt
 sudo mkdir /etc/apt/sources.list.d
-
 sudo touch /etc/apt/sources.list.d/docker.list
 sudo cat > /etc/apt/sources.list.d/docker.list << EOF
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
@@ -23,9 +22,9 @@ sudo usermod -aG docker asd
 touch /etc/init.d/myscript.sh
 sudo cat > /etc/init.d/myscript.sh << EOF
 sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.0.1 http://192.168.1.3:8080/v1/scripts/8FD1B57205383841D6C6:1465902000000:K7K3C5C5hUHt4yeP3PIO79ELsc
-sudo chmod +x /mos/ping.sh
 sudo mkdir /mos
 sudo wget -P /mos/ --no-check-certificate https://raw.githubusercontent.com/Supermies/Testata/master/ping.sh
+sudo chmod +x /mos/ping.sh
 sudo rm /etc/init.f/myscript.sh
 EOF
 sudo chmod +x /etc/init.d/myscript.sh
