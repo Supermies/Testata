da784dfd05aac3a47de2b8951bee909cc52fe833
Supermies <pohjola.ville@suomi24.fi>
2016-07-25 06:38:19 -0400

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,42 +1,4 @@
 #!/bin/bash
-yes | lvremove VG
-yes | vgremove VG
-pvcreate /dev/sda 
-vgcreate VG /dev/sda
-pvcreate /dev/sda1 
-vgcreate VG /dev/sda1
-vgextend VG /dev/sda1
-pvcreate /dev/sda2 
-vgcreate VG /dev/sda2
-vgextend VG /dev/sda2
-pvcreate /dev/sda3 
-vgcreate VG /dev/sda3
-vgextend VG /dev/sda3
-pvcreate /dev/sda4 
-vgcreate VG /dev/sda4
-vgextend VG /dev/sda4
-pvcreate /dev/sda5 
-vgcreate VG /dev/sda5
-vgextend VG /dev/sda5
-pvcreate /dev/sdb 
-vgcreate VG /dev/sdb
-vgextend VG /dev/sdb
-pvcreate /dev/sdb1
-vgcreate VG /dev/sdb1
-vgextend VG /dev/sdb1
-pvcreate /dev/sdb2
-vgcreate VG /dev/sdb2
-vgextend VG /dev/sdb2
-pvcreate /dev/sdb3 
-vgcreate VG /dev/sdb3
-vgextend VG /dev/sdb3
-pvcreate /dev/sdb4 
-vgcreate VG /dev/sdb4
-vgextend VG /dev/sdb4
-yes | lvcreate -l 100%FREE -n levy VG 
-yes | mkfs.ext3 /dev/VG/levy 
-mkdir /kansio
-mount /dev/VG/levy /kansio
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https ca-certificates -y
