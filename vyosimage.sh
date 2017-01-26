a6c1986a6bdc2860ad0a884fc196c0ebea58f331
Supermies <pohjola.ville@suomi24.fi>
2017-01-26 13:01:51 +0200

    Update vyosimage.sh

--- vyosimage.sh
+++ vyosimage.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 sudo apt-get update
 sudo mkdir /vyos
-sudo cd /vyos
-sudo wget http://packages.vyos.net/iso/release/1.1.7/vyos-1.1.7-amd64.iso
+sudo wget http://packages.vyos.net/iso/release/1.1.7/vyos-1.1.7-amd64.iso /vyos
 mkdir /rootfs
 sudo mount -o loop /vyos/vyos-1.1.7-amd64.iso /rootfs
 sudo apt-get install -y squashfs-tools
 mkdir /unsquashfs
-sudo unsquashfs -f -d /unsquashfs /rootfs/install/filesystem.squashfs
-sudo tar -C unsquahsfs -c . | docker import – meidan/vyos
+sudo unsquashfs -f -d /unsquashfs /rootfs/live/filesystem.squashfs
+sudo tar -C /unsquashfs -c . | docker import – meidan/vyos
