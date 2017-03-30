60e8939ca88b6c36770479d3e837908d64b86192
Supermies <pohjola.ville@suomi24.fi>
2017-03-30 15:33:32 +0300

    Update contreg.sh

--- contreg.sh
+++ contreg.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 openssl genrsa -out domain.key 4096
-openssl req -new -x509 -text -key domain.key -out domain.crt
+openssl req -new -x509 -text -key client.key -out client.cert
 mkdir /certs
-mv domain.key /certs
-mv domain.crt /certs
-docker run -d -p 5000:5000 --restart=always --name registry   -v `pwd`/certs:/certs   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt   -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key   registry:2
-docker pull mongo && docker tag mongo registry:5000/mongo
-docker push registry:5000/mongo
-docker pull redis && docker tag redis registry:5000/redis
-docker push registry:5000/redis
-docker pull n4sjamk/teamboard-api && docker tag n4sjamk/teamboard-api registry:5000/teamboard-api
-docker push registry:5000/teamboard-api
-docker pull n4sjamk/teamboard-io && docker tag n4sjamk/teamboard-io registry:5000/teamboard-io
-docker push registry:5000/teamboard-io
-docker pull n4sjamk/teamboard-client && docker tag n4sjamk/teamboard-client registry:5000/teamboard-client
-docker push registry:5000/teamboard-client
+mv client.key /certs
+mv client.cert /certs
+mkdir /etc/docker/certs.d
+mkdir /etc/docker/certs.d/registry.loc:5000
+cp /certs/client.key /etc/docker/certs.d/registry.loc:5000
+cp /certs/client.cert /etc/docker/certs.d/registry.loc:5000/client.cert
+docker run -d -p 5000:5000 --restart=always --name registry   -v `pwd`/certs:/certs   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/client.crt   -e REGISTRY_HTTP_TLS_KEY=/certs/client.key   registry:2
+service docker restart
+docker pull mongo && docker tag mongo registry.loc:5000/mongo
+docker push registry.loc:5000/mongo
+docker pull redis && docker tag redis registry.loc:5000/redis
+docker push registry.loc:5000/redis
+docker pull n4sjamk/teamboard-api && docker tag n4sjamk/teamboard-api registry.loc:5000/teamboard-api
+docker push registry.loc:5000/teamboard-api
+docker pull n4sjamk/teamboard-io && docker tag n4sjamk/teamboard-io registry.loc:5000/teamboard-io
+docker push registry.loc:5000/teamboard-io
+docker pull n4sjamk/teamboard-client && docker tag n4sjamk/teamboard-client registry.loc:5000/teamboard-client
+docker push registry.loc:5000/teamboard-client
